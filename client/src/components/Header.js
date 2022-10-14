a8873bd5c9db4f3949907a6636141b72d89d24c5
cw417 <chriswhitlock417@gmail.com>
2022-10-14 01:22:24 -0400

    Add key press event watcher

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,12 +8,23 @@ export default function Header({ name, updateName }) {
     updateName(nameRef.current.value);
   }
 
+  function handleKeyPress(event) {
+    /**
+     * Watches for 'Enter' keypress.
+     * @param event  Keypress event watching for 'Enter' key.
+     */
+    if (event.keyCode === 13 || event.which === 13) {
+      handleUpdateName();
+    }
+  }
+
+
   return (
     <div className='header'>
       <div style={{display:(name === "") ? "none" : "block"}}>Hello, {name}!</div>
       <div style={{display:(name === "") ? "block" : "none"}}>
         <label className='name-input--label'>What is your name?</label>
-        <input className="name-input--input" ref={nameRef} type="text" />
+        <input className="name-input--input" ref={nameRef} onKeyUp={handleKeyPress} type="text" />
         <button className='name-input--button' onClick={handleUpdateName}>Update</button>
       </div>
     </div>
