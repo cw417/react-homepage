d0f8de64faab503a6ea1ec5932421f5814e6b4da
cw417 <chriswhitlock417@gmail.com>
2022-10-14 01:17:01 -0400

    Work on header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,13 +9,13 @@ export default function Header({ name, updateName }) {
   }
 
   return (
-    <>
+    <div className='header'>
       <div style={{display:(name === "") ? "none" : "block"}}>Hello, {name}!</div>
       <div style={{display:(name === "") ? "block" : "none"}}>
-        <label>What is your name?</label>
-        <input ref={nameRef} type="text" />
-        <button onClick={handleUpdateName}>Add</button>
+        <label className='name-input--label'>What is your name?</label>
+        <input className="name-input--input" ref={nameRef} type="text" />
+        <button className='name-input--button' onClick={handleUpdateName}>Update</button>
       </div>
-    </>
+    </div>
   )
 }
