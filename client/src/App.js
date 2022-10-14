01ae8ccd77e9fa284729d6dde3d4ccae2691b23e
cw417 <chriswhitlock417@gmail.com>
2022-10-14 02:10:36 -0400

    Weather API access working

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,36 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [ userInfo, setUserInfo ] = useState({ name: "" });
+  const [ userInfo, setUserInfo ] = useState({ name: "", location: "toronto" });
+  const [ weatherApiData, setWeatherApiData ] = useState()
   
   // local storage setup
+
   const LOCAL_STORAGE_KEY = 'homepageApp.info'
 
-  // local storage setup
   useEffect(() => {
     const storedUserInfo = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (storedUserInfo) setUserInfo(storedUserInfo);
   }, []);
 
-  // local storage setup
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(userInfo))
   }, [userInfo]);
+
+  // weather api setup
+
+  const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const lat = 43.7001;
+  const lon = -79.4163;
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`
+
+  useEffect(() => {
+    fetch(weatherApiUrl)
+      .then((res) => res.json())
+      .then((data) => setWeatherApiData(data));
+  }, [weatherApiUrl]);
+
+  // functions
 
   function updateName(newName) {
     const newUserInfo = {...userInfo, name: newName};
@@ -34,6 +49,7 @@ function App() {
         <Weather />
         <Todos />
       </div>
+      <div>{JSON.stringify(weatherApiData)}</div>
     </div>
   );
 }
